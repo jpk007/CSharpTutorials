20e4e9d2a303ead7e855718407948a0dfb05bd32
jpk007 <jayeshjayaprakash23@gmail.com>
2024-06-20 22:29:16 +0530

    changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World!!";
+
+            Console.WriteLine(message);
+        }
+    }
+}
